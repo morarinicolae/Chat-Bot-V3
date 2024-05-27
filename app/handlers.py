22d15fae840304a05f1097dadfa7656b1cf4d752
Nicolae <morari.nicolae1998@gmail.com>
2024-05-27 18:36:42 +0300

    unknown message

--- app/handlers.py
+++ app/handlers.py
@@ -8,10 +8,15 @@ import app.database.requests as rq
 router = Router()
 
 
+@router.callback_query(lambda c: c.data == 'to_main')
+async def handle_to_main(callback_query: CallbackQuery):
+    await callback_query.message.answer("Selecteaza te rog din menu cu ce as putea sa te ajut?")
+    await callback_query.answer()
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await rq.set_user(message.from_user.id)
-    await message.answer('Salut! Aici poti gasi locurile ce poti sa le vizitezi!', reply_markup=kb.main)
+    await message.answer('Salut! Aici poti gasi locurile ce poti sa le vizitezi! Alege din menu cu ce te-as putea ajuta', reply_markup=kb.main)
 
 
 @router.message(F.text == 'Catalog')
@@ -31,3 +36,7 @@ async def category(callback: CallbackQuery):
     item_data = await rq.get_item(callback.data.split('_')[1])
     await callback.answer('Ați ales produsul')
     await callback.message.answer(f'Titlu: {item_data.name}\nDescriere: {item_data.description}\nPreț: {item_data.price}')
+
+@router.message(F.text)
+async def understand(message:Message):
+    await message.answer('Nu inteleg, incearca te rog sa scrii comanda /start')
